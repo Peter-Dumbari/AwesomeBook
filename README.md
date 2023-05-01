39250616b017ddb8ec65bb3a2961df1f307763ec
TAMS-TECHNOLOGY <dummytwiz@gmail.com>
2023-05-01 14:58:49 +0100

    Updates the MIT.md README.md and app.js files

--- README.md
+++ README.md
@@ -1,40 +1,12 @@
 <a name="readme-top"></a>
 
-<!--
-HOW TO USE:
-This is an example of how you may give instructions on setting up your project locally.
-
-Modify this file to match your project and remove sections that don't apply.
-
-REQUIRED SECTIONS:
-- Table of Contents
-- About the Project
-  - Built With
-  - Live Demo
-- Getting Started
-- Authors
-- Future Features
-- Contributing
-- Show your support
-- Acknowledgements
-- License
-
-OPTIONAL SECTIONS:
-- FAQ
-
-After you're finished please remove all the comments and instructions!
--->
-
 <div align="center">
-  <!-- You are encouraged to replace this logo with your own! Otherwise you can also remove it. -->
   <img src="murple_logo.png" alt="logo" width="140"  height="auto" />
   <br/>
 
   <h3><b>Microverse README Template</b></h3>
 
 </div>
-
-<!-- TABLE OF CONTENTS -->
 
 # 📗 Table of Contents
 
@@ -58,38 +30,23 @@ After you're finished please remove all the comments and instructions!
 - [❓ FAQ (OPTIONAL)](#faq)
 - [📝 License](#license)
 
-<!-- PROJECT DESCRIPTION -->
+# 📖 [Awesome Books] <a name="about-project"></a>
 
-# 📖 [your_project_name] <a name="about-project"></a>
-
-> Describe your project in 1 or 2 sentences.
-
-**[your_project__name]** is a...
+**[Awesome Books]** is a online store for storing names, author and description of books
 
 ## 🛠 Built With <a name="built-with"></a>
 
 ### Tech Stack <a name="tech-stack"></a>
 
-> Describe the tech stack and include only the relevant sections that apply to your project.
+###
 
 <details>
   <summary>Client</summary>
   <ul>
-    <li><a href="https://reactjs.org/">React.js</a></li>
-  </ul>
-</details>
-
-<details>
-  <summary>Server</summary>
-  <ul>
-    <li><a href="https://expressjs.com/">Express.js</a></li>
-  </ul>
-</details>
-
-<details>
-<summary>Database</summary>
-  <ul>
-    <li><a href="https://www.postgresql.org/">PostgreSQL</a></li>
+    <li><a href="">HTML</a></li>
+    <li><a href="">CSS</a></li>
+    <li><a href="">JAVASCRIPT</a></li>
+    <li><a href="">LINTERS</a></li>
   </ul>
 </details>
 
@@ -97,29 +54,19 @@ After you're finished please remove all the comments and instructions!
 
 ### Key Features <a name="key-features"></a>
 
-> Describe between 1-3 key features of the application.
-
-- **[key_feature_1]**
-- **[key_feature_2]**
-- **[key_feature_3]**
+- **[Add Book]**
+- **[Display books]**
+- **[Remove book]**
 
 <p align="right">(<a href="#readme-top">back to top</a>)</p>
-
-<!-- LIVE DEMO -->
 
 ## 🚀 Live Demo <a name="live-demo"></a>
 
-> Add a link to your deployed project.
-
-- [Live Demo Link](https://google.com)
+- [Live Demo Link](https://menendezon.github.io/awesome-books/)
 
 <p align="right">(<a href="#readme-top">back to top</a>)</p>
 
-<!-- GETTING STARTED -->
-
 ## 💻 Getting Started <a name="getting-started"></a>
-
-> Describe how a new developer could make use of your project.
 
 To get a local copy up and running, follow these steps.
 
@@ -137,101 +84,57 @@ Example command:
 
 ### Setup
 
-Clone this repository to your desired folder:
-
-<!--
-Example commands:
-
-```sh
-  cd my-folder
-  git clone git@github.com:myaccount/my-project.git
-```
---->
-
-### Install
-
-Install this project with:
-
-<!--
-Example command:
-
-```sh
-  cd my-project
-  gem install
-```
---->
-
-### Usage
-
-To run the project, execute the following command:
-
-<!--
-Example command:
-
-```sh
-  rails server
-```
---->
+-Create a new GitHub repo.<br>
+-Create a new branch according to the GitHub flow rules.<br>
+-Add a .gitignore file.<br>
+-Add node_modules to gitignore file.<br>
+-Set up a linter for HTML and CSS and make sure that any files that are not relevant are ignored by git.<br>
+-Add descriptive README file to your project - please use this template but remember to customize it to your project.<br>
+-Add 3 files (each one in a separate commit):<br>
+-A .html file.<br>
+-A .css file..<br>
+-A .js file..<br>  
+-Clone this repository to your desired folder:
 
 ### Run tests
 
 To run tests, run the following command:
 
-<!--
-Example command:
+```sh
+  npx hint .
+```
 
 ```sh
-  bin/rails test test/models/article_test.rb
+  npx stylelint "**/*.{css,scss}"
 ```
---->
 
 ### Deployment
 
-You can deploy this project using:
-
-<!--
-Example:
-
-```sh
-
-```
- -->
+This project is deployed at github pages you can find it at: https://github.com/MenendezON/awesome-books.git
 
 <p align="right">(<a href="#readme-top">back to top</a>)</p>
-
-<!-- AUTHORS -->
 
 ## 👥 Authors <a name="authors"></a>
 
-> Mention all of the collaborators of this project.
+👤 **MENENDEZ NELSON**
 
-👤 **Author1**
+- GitHub: [@menendezon](https://github.com/menendezon)
+- Twitter: [@menendezon](https://twitter.com/menendezon)
+- LinkedIn: [MenendezON](https://linkedin.com/in/menendezon)
 
-- GitHub: [@githubhandle](https://github.com/githubhandle)
-- Twitter: [@twitterhandle](https://twitter.com/twitterhandle)
-- LinkedIn: [LinkedIn](https://linkedin.com/in/linkedinhandle)
+👤 **PETER DUMBARI**
 
-👤 **Author2**
-
-- GitHub: [@githubhandle](https://github.com/githubhandle)
-- Twitter: [@twitterhandle](https://twitter.com/twitterhandle)
-- LinkedIn: [LinkedIn](https://linkedin.com/in/linkedinhandle)
+- GitHub: [@Peter-Dumbari](https://github.com/github.com/peter-dumbari)
 
 <p align="right">(<a href="#readme-top">back to top</a>)</p>
-
-<!-- FUTURE FEATURES -->
 
 ## 🔭 Future Features <a name="future-features"></a>
 
-> Describe 1 - 3 features you will add to the project.
-
-- [ ] **[new_feature_1]**
-- [ ] **[new_feature_2]**
-- [ ] **[new_feature_3]**
+- [ ] **[Edit book]**
+- [ ] **[Popup Page]**
+- [ ] **[Upload Image]**
 
 <p align="right">(<a href="#readme-top">back to top</a>)</p>
-
-<!-- CONTRIBUTING -->
 
 ## 🤝 Contributing <a name="contributing"></a>
 
@@ -241,21 +144,13 @@ Feel free to check the [issues page](../../issues/).
 
 <p align="right">(<a href="#readme-top">back to top</a>)</p>
 
-<!-- SUPPORT -->
-
 ## ⭐️ Show your support <a name="support"></a>
 
-> Write a message to encourage readers to support your project
-
-If you like this project...
+If you like this project Please drop a comment🤗
 
 <p align="right">(<a href="#readme-top">back to top</a>)</p>
 
-<!-- ACKNOWLEDGEMENTS -->
-
 ## 🙏 Acknowledgments <a name="acknowledgements"></a>
-
-> Give credit to everyone who inspired your codebase.
 
 I would like to thank...
 
@@ -265,23 +160,15 @@ I would like to thank...
 
 ## ❓ FAQ (OPTIONAL) <a name="faq"></a>
 
-> Add at least 2 questions new developers would ask when they decide to use your project.
+- **[How will this project be of benefit to me?]**
 
-- **[Question_1]**
-
-  - [Answer_1]
-
-- **[Question_2]**
-
-  - [Answer_2]
+  - [This project will definitely help in storing the names, author and the description of your books free of charge]
 
 <p align="right">(<a href="#readme-top">back to top</a>)</p>
 
-<!-- LICENSE -->
-
 ## 📝 License <a name="license"></a>
 
-This project is [MIT](./LICENSE) licensed.
+This project is [MIT](./MIT.md) licensed.
 
 _NOTE: we recommend using the [MIT license](https://choosealicense.com/licenses/mit/) - you can set it up quickly by [using templates available on GitHub](https://docs.github.com/en/communities/setting-up-your-project-for-healthy-contributions/adding-a-license-to-a-repository). You can also use [any other license](https://choosealicense.com/licenses/) if you wish._
 
--- public/js/app.js
+++ public/js/app.js
@@ -1,52 +1,148 @@
-const menu = document.querySelectorAll('.item-menu');
-const h1 = document.querySelector('h1');
-const contain = document.querySelector('.contain');
-const currentDate = document.querySelector('.current-date p');
+const menu = document.querySelectorAll(".item-menu");
+const h1 = document.querySelector("h1");
+const contain = document.querySelector(".contain");
+const currentDate = document.querySelector(".current-date p");
 let books = [];
 
 /* eslint-disable no-unused-vars */
 const menuActive = (menu, active) => {
   for (let i = 0; i < menu.length; i += 1) {
-    menu[i].classList.remove('active');
+    menu[i].classList.remove("active");
   }
-  active.classList.add('active');
+  active.classList.add("active");
 };
-
 const generateTable = () => {
-  contain.innerHTML = 'List of book content';
+  contain.innerHTML = "";
+  const table = document.createElement("table");
+  // header
+  const tr = document.createElement("tr");
+  const th1 = document.createElement("th");
+  th1.textContent = "#";
+  const th2 = document.createElement("th");
+  th2.textContent = "Title";
+  const th3 = document.createElement("th");
+  th3.textContent = "Author";
+  const th4 = document.createElement("th");
+  th4.textContent = "Created";
+  const th5 = document.createElement("th");
+  th5.textContent = "Actions";
+  tr.appendChild(th1);
+  tr.appendChild(th2);
+  tr.appendChild(th3);
+  tr.appendChild(th4);
+  tr.appendChild(th5);
+  table.appendChild(tr);
+  // body
+  books.forEach((book, i) => {
+    const tr1 = document.createElement("tr");
+    const th11 = document.createElement("th");
+    th11.textContent = i + 1;
+    const th12 = document.createElement("td");
+    th12.textContent = book.title;
+    const th13 = document.createElement("td");
+    th13.textContent = book.author;
+    const th14 = document.createElement("td");
+    th14.textContent = book.created_at;
+    const th15 = document.createElement("td");
+    th15.innerHTML =
+      '<a href=""><i class="fa fa-search-plus" aria-hidden="true"></i></a> | <a href="#" onclick="removeBook(' +
+      i +
+      ')"><i class="fa fa-trash-o" aria-hidden="true"></i></a>';
+    tr1.appendChild(th11);
+    tr1.appendChild(th12);
+    tr1.appendChild(th13);
+    tr1.appendChild(th14);
+    tr1.appendChild(th15);
+    table.appendChild(tr1);
+  });
+  contain.appendChild(table);
 };
-
 const generateAddForm = () => {
-  contain.innerHTML = 'Add new book content';
+  const parent = document.createElement("form");
+  parent.setAttribute("action", "");
+  parent.setAttribute("method", "POST");
+  parent.setAttribute("onsubmit", "return validateForm()");
+  parent.setAttribute("id", "add_book");
+  const l1 = document.createElement("fieldset");
+  l1.innerHTML = '<legend><label for="title">Title</label></legend>';
+  parent.appendChild(l1);
+  // input title
+  const input = document.createElement("input");
+  input.setAttribute("type", "text");
+  input.setAttribute("name", "title");
+  input.setAttribute("id", "title");
+  input.setAttribute("placeholder", "Write the title");
+  input.setAttribute("value", "Book 1");
+  l1.appendChild(input);
+  const l2 = document.createElement("fieldset");
+  l2.innerHTML = '<legend><label for="author">Author</label></legend>';
+  parent.appendChild(l2);
+  // input author
+  const input2 = document.createElement("input");
+  input2.setAttribute("type", "text");
+  input2.setAttribute("name", "author");
+  input2.setAttribute("id", "author");
+  input2.setAttribute("placeholder", "Write the author");
+  input2.setAttribute("value", "Author 1");
+  l2.appendChild(input2);
+  // input submit
+  const input3 = document.createElement("input");
+  input3.setAttribute("type", "submit");
+  input3.setAttribute("value", "Add");
+  parent.appendChild(input3);
+  contain.innerHTML = "";
+  contain.appendChild(parent);
 };
-
 const generateContactForm = () => {
-  contain.innerHTML = 'Contact content';
+  const parentElement = document.createElement("div");
+  parentElement.classList.add("contact");
+  const childElement1 = document.createElement("p");
+  const textNode = document.createTextNode(
+    'Do you have any questions or you just want to say "Hello"?\r\nYou can reach out to us!'
+  );
+  childElement1.appendChild(textNode);
+  const list = document.createElement("ul");
+  const item1 = document.createElement("li");
+  item1.innerHTML =
+    '<strong>Our e-mail : </strong><a href="mailto:contact@awesomebooks.org">contact@awesomebooks.org</a>';
+  const item2 = document.createElement("li");
+  item2.innerHTML =
+    '<strong>Our phone number: </strong><a href="tel:+221 78 012 3456">+221 78 012 3456</a>';
+  const item3 = document.createElement("li");
+  item3.innerHTML =
+    "<strong>Our address: </strong>Street name 22, 84503 City, Country";
+  list.appendChild(item1);
+  list.appendChild(item2);
+  list.appendChild(item3);
+  parentElement.appendChild(childElement1);
+  parentElement.appendChild(list);
+  contain.innerHTML = "";
+  contain.appendChild(parentElement);
 };
 
 const removeBook = (index) => {
   books = books.filter((book) => book.title !== books[index].title);
-  localStorage.setItem('books', JSON.stringify(books));
+  localStorage.setItem("books", JSON.stringify(books));
   generateTable();
 };
 
-const changeTitle = (itemMenu = '#page-0') => {
-  let text = '';
+const changeTitle = (itemMenu = "#page-0") => {
+  let text = "";
   switch (itemMenu) {
-    case '#page-0':
-      text = 'All awesome books';
+    case "#page-0":
+      text = "All awesome books";
       generateTable();
       break;
-    case '#page-1':
-      text = 'Add a new book';
+    case "#page-1":
+      text = "Add a new book";
       generateAddForm();
       break;
-    case '#page-2':
-      text = 'Contact information';
+    case "#page-2":
+      text = "Contact information";
       generateContactForm();
       break;
     default:
-      text = 'Contact information';
+      text = "Contact information";
       generateContactForm();
       break;
   }
@@ -54,8 +150,8 @@ const changeTitle = (itemMenu = '#page-0') => {
 };
 
 menu.forEach((x, i) => {
-  menu[i].addEventListener('click', () => {
-    changeTitle(menu[i].getAttribute('href'));
+  menu[i].addEventListener("click", () => {
+    changeTitle(menu[i].getAttribute("href"));
     menuActive(menu, menu[i]);
   });
 });
@@ -63,29 +159,34 @@ menu.forEach((x, i) => {
 const formatDate = () => {
   const today = new Date();
   const options = {
-    year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric',
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
   };
-  return today.toLocaleDateString('en-US', options);
+  return today.toLocaleDateString("en-US", options);
 };
 
 const generateCurrentDate = () => {
   currentDate.innerHTML = formatDate();
 };
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   setInterval(generateCurrentDate, 1000);
   changeTitle();
   menuActive(menu, menu[0]);
 
-  if (localStorage.getItem('books')) {
-    books = JSON.parse(localStorage.getItem('books'));
+  if (localStorage.getItem("books")) {
+    books = JSON.parse(localStorage.getItem("books"));
   }
   generateTable();
 });
 
 const validateForm = () => {
-  const title = document.getElementById('title');
-  const author = document.getElementById('author');
+  const title = document.getElementById("title");
+  const author = document.getElementById("author");
 
   const newBook = {
     title: title.value,
@@ -94,10 +195,10 @@ const validateForm = () => {
   };
 
   const success = books.push(newBook);
-  localStorage.setItem('books', JSON.stringify(books));
+  localStorage.setItem("books", JSON.stringify(books));
   if (success >= books.length) {
-    title.value = '';
-    author.value = '';
+    title.value = "";
+    author.value = "";
     generateTable();
     return true;
   }
