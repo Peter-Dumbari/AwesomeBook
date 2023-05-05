f27d7707808e7b6e64bafcaa955def884ce47ce9
TAMS-TECHNOLOGY <dummytwiz@gmail.com>
2023-05-05 15:44:35 +0100

    updates

--- public/js/app.js
+++ public/js/app.js
@@ -1,61 +1,95 @@
 const menu = document.querySelectorAll('.item-menu');
+const modal = document.querySelectorAll('.modal');
+const dismiss = document.querySelectorAll('.dismiss');
 const h1 = document.querySelector('h1');
 const contain = document.querySelector('.contain');
 const currentDate = document.querySelector('.current-date p');
-let books = [];
 
-/* eslint-disable no-unused-vars */
+const upLoadFile = document.querySelector('#cover');
+
+class ListOfBooks {
+  books;
+
+  constructor() {
+    this.getData();
+  }
+
+  getData = () => {
+    const data = localStorage.getItem('datas');
+    if (data) {
+      this.books = JSON.parse(data);
+    } else {
+      this.books = [];
+    }
+  };
+
+  setLocalStorage = (newBooks) => localStorage.setItem('datas', JSON.stringify(newBooks));
+
+  addBook = (newBook) => {
+    this.books.push(newBook);
+    this.setLocalStorage(this.books);
+  };
+
+  removeBook = (index) => {
+    const filtered = this.books.filter(
+      (book) => book.title !== this.books[index].title,
+    );
+    localStorage.setItem('datas', JSON.stringify(filtered));
+  };
+
+  generateTable = () => {
+    contain.innerHTML = '';
+    const table = document.createElement('table');
+    // header
+    const tr = document.createElement('tr');
+    const th1 = document.createElement('th');
+    th1.textContent = '#';
+    const th2 = document.createElement('th');
+    th2.textContent = 'Title';
+    const th3 = document.createElement('th');
+    th3.textContent = 'Author';
+    const th4 = document.createElement('th');
+    th4.textContent = 'Created';
+    const th5 = document.createElement('th');
+    th5.textContent = 'Actions';
+    tr.appendChild(th1);
+    tr.appendChild(th2);
+    tr.appendChild(th3);
+    tr.appendChild(th4);
+    tr.appendChild(th5);
+    table.appendChild(tr);
+    // body
+    this.books.forEach((bk, i) => {
+      const tr1 = document.createElement('tr');
+      const th11 = document.createElement('th');
+      th11.textContent = i + 1;
+      const th12 = document.createElement('td');
+      th12.textContent = bk.title;
+      const th13 = document.createElement('td');
+      th13.textContent = bk.author;
+      const th14 = document.createElement('td');
+      th14.textContent = bk.created_at;
+      const th15 = document.createElement('td');
+      th15.innerHTML = `
+      <a href="#" class="modal" id="${i}" onclick="popupBook(${i})"><i class="fa fa-search-plus" aria-hidden="true"></i></a> | 
+      <a href="#" class="dismiss" id="${i}" onclick="removeBook(${i})"><i class="fa fa-trash-o" aria-hidden="true"></i></a>`;
+      tr1.appendChild(th11);
+      tr1.appendChild(th12);
+      tr1.appendChild(th13);
+      tr1.appendChild(th14);
+      tr1.appendChild(th15);
+      table.appendChild(tr1);
+    });
+    contain.appendChild(table);
+  };
+}
+const listofbooks = new ListOfBooks();
+
 const menuActive = (menu, active) => {
   for (let i = 0; i < menu.length; i += 1) {
     menu[i].classList.remove('active');
   }
   active.classList.add('active');
-};
-
-const generateTable = () => {
-  contain.innerHTML = '';
-  const table = document.createElement('table');
-  // header
-  const tr = document.createElement('tr');
-  const th1 = document.createElement('th');
-  th1.textContent = '#';
-  const th2 = document.createElement('th');
-  th2.textContent = 'Title';
-  const th3 = document.createElement('th');
-  th3.textContent = 'Author';
-  const th4 = document.createElement('th');
-  th4.textContent = 'Created';
-  const th5 = document.createElement('th');
-  th5.textContent = 'Actions';
-  tr.appendChild(th1);
-  tr.appendChild(th2);
-  tr.appendChild(th3);
-  tr.appendChild(th4);
-  tr.appendChild(th5);
-  table.appendChild(tr);
-  // body
-  books.forEach((book, i) => {
-    const tr1 = document.createElement('tr');
-    const th11 = document.createElement('th');
-    th11.textContent = i + 1;
-    const th12 = document.createElement('td');
-    th12.textContent = book.title;
-    const th13 = document.createElement('td');
-    th13.textContent = book.author;
-    const th14 = document.createElement('td');
-    th14.textContent = book.created_at;
-    const th15 = document.createElement('td');
-    th15.innerHTML = `
-    <a href="#" onclick="popupBook(${i})"><i class="fa fa-search-plus" aria-hidden="true"></i></a> | 
-    <a href="#" onclick="removeBook(${i})"><i class="fa fa-trash-o" aria-hidden="true"></i></a>`;
-    tr1.appendChild(th11);
-    tr1.appendChild(th12);
-    tr1.appendChild(th13);
-    tr1.appendChild(th14);
-    tr1.appendChild(th15);
-    table.appendChild(tr1);
-  });
-  contain.appendChild(table);
 };
 
 const generateAddForm = () => {
@@ -127,18 +161,12 @@ const generateContactForm = () => {
   contain.appendChild(parentElement);
 };
 
-const removeBook = (index) => {
-  books = books.filter((book) => book.title !== books[index].title);
-  localStorage.setItem('books', JSON.stringify(books));
-  generateTable();
-};
-
 const changeTitle = (itemMenu = '#page-0') => {
   let text = '';
   switch (itemMenu) {
     case '#page-0':
       text = 'All awesome books';
-      generateTable();
+      listofbooks.generateTable();
       break;
     case '#page-1':
       text = 'Add a new book';
@@ -180,20 +208,22 @@ const generateCurrentDate = () => {
   currentDate.innerHTML = formatDate();
 };
 
-window.addEventListener('DOMContentLoaded', (event) => {
+window.addEventListener('DOMContentLoaded', () => {
   setInterval(generateCurrentDate, 1000);
   changeTitle();
   menuActive(menu, menu[0]);
-  if (localStorage.getItem('books')) {
-    books = JSON.parse(localStorage.getItem('books'));
-  }
 
-  generateTable();
+  listofbooks.books = JSON.parse(localStorage.getItem('datas')) ?? [];
+  listofbooks.generateTable();
 });
 
 const validateForm = (coverImg = '') => {
   const title = document.getElementById('title');
   const author = document.getElementById('author');
+
+  if (coverImg.trim().length === 0) {
+    coverImg = './public/images/default.png';
+  }
 
   const newBook = {
     title: title.value,
@@ -202,15 +232,10 @@ const validateForm = (coverImg = '') => {
     cover: coverImg,
     description: '',
   };
+  title.value = '';
+  author.value = '';
 
-  const success = books.push(newBook);
-  localStorage.setItem('books', JSON.stringify(books));
-  if (success >= books.length) {
-    title.value = '';
-    author.value = '';
-    generateTable();
-    return true;
-  }
+  listofbooks.addBook(newBook);
   return false;
 };
 
@@ -225,6 +250,16 @@ const updloadImage = () => {
   }
 };
 
+upLoadFile?.addEventListener('change', () => {
+  updloadImage();
+});
+
+const removeBook = (index) => {
+  listofbooks.removeBook(index);
+  listofbooks.getData();
+  listofbooks.generateTable();
+};
+
 const popupBook = (index) => {
   const back = document.createElement('div');
   back.classList.add('fullScreen');
@@ -232,17 +267,18 @@ const popupBook = (index) => {
   popup.classList.add('popup');
 
   const side0 = document.createElement('div');
-  side0.innerHTML = `<img src="${books[index].cover}" alt="">`;
+  side0.innerHTML = `<img src="${listofbooks.books[index].cover}" alt="">`;
   popup.appendChild(side0);
 
   const side1 = document.createElement('div');
-  side1.innerHTML = `<h2>${books[index].title}</h2><h3><em>by ${books[index].author}</em></h3><p><em>added ${books[index].created_at}</em></p><p>${books[index].description}</p>`;
+  side1.innerHTML = `<h2>${listofbooks.books[index].title}</h2><h3><em>by ${listofbooks.books[index].author}</em></h3><p><em>added ${listofbooks.books[index].created_at}</em></p><p>${listofbooks.books[index].description}</p>`;
   popup.appendChild(side1);
 
   const link = document.createElement('a');
   link.innerText = 'See less';
   link.setAttribute('onclick', 'closeBtn()');
   link.setAttribute('href', '#');
+  link.setAttribute('id', 'closeBtn');
   side1.appendChild(link);
 
   back.appendChild(popup);
@@ -250,7 +286,25 @@ const popupBook = (index) => {
   return false;
 };
 
+const close = document.getElementById('closeBtn');
+
 const closeBtn = () => {
   document.body.removeChild(document.querySelector('.fullScreen'));
   return false;
 };
+
+close?.addEventListener('click', () => {
+  closeBtn();
+});
+
+modal.forEach((x, i) => {
+  modal[i].addEventListener('click', () => {
+    popupBook(i);
+  });
+});
+
+dismiss.forEach((x, i) => {
+  dismiss[i].addEventListener('click', () => {
+    removeBook(i);
+  });
+});
